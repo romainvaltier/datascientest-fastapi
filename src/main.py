103132af5e799713d004729efb8714ca46d4b6d5
Romain Valtier <romain@valtier.fr>
2022-10-10 16:02:04 +0200

    Update

--- src/main.py
+++ src/main.py
@@ -2,12 +2,76 @@ from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from typing import Optional
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from passlib.context import CryptContext
 import pandas as pd
 
-api = FastAPI()
+description = """
+Datascientest-fastapi API helps you do awesome stuff. 🚀
+
+## User
+
+You will be able to:
+* Get current **user**.
+
+## Use
+
+You will be able to:
+* Get a list of **use** and associated **subject(s)**.
+
+## Questions
+
+You will be able to:
+* Get a list of all **questions**
+
+## Question
+
+You will be able to:
+* Add one **question** to existing set of questions.
+
+## Exam
+
+Ypu will be able to:
+
+* Generate a random set of questions for an **exam** with specific use and a list of associated subject(s).
+"""
+
+api = FastAPI(title="Datascientest-fastapi exam", description=description)
+
+security = HTTPBasic()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # user database
-users_db = {"alice": "wonderland", "bob": "builder", "clementine": "mandarine"}
+# users_db = {"alice": "wonderland", "bob": "builder", "clementine": "mandarine"}
+users = {
+    "alice": {
+        "username": "alice",
+        "hashed_password": pwd_context.hash("wonderland"),
+    },
+    "bob": {
+        "username": "bob",
+        "hashed_password": pwd_context.hash("builder"),
+    },
+    "clementine": {
+        "username": "clementine",
+        "hashed_password": pwd_context.hash("mandarine"),
+    },
+}
+
+
+def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    username = credentials.username
+    if not (users.get(username)) or not (
+        pwd_context.verify(credentials.password, users[username]["hashed_password"])
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
 
 # questions database
 questions_db = pd.read_csv(
@@ -21,7 +85,12 @@ questions_dict = questions_db.to_dict(orient="records")
 
 @api.get("/")
 async def get_index():
-    return {"grettings": "Welcome!"}
+    return {"message": "Welcome to {} !".format(api.title)}
+
+
+@api.get("/user")
+def current_user(username: str = Depends(get_current_user)):
+    return "Hello {}".format(username)
 
 
 @api.get("/use")
@@ -48,22 +117,6 @@ async def get_questions():
         return {}
 
 
-@api.get("/exam")
-async def get_exam(use: str, subject: str, nb: int):
-    try:
-        subject_lst = list(subject.split(","))
-        questions_db = pd.DataFrame(jsonable_encoder(questions_dict))
-        questions_lst = questions_db[
-            (questions_db["use"] == use) & (questions_db["subject"].isin(subject_lst))
-        ]
-        if nb > len(questions_lst):
-            nb = len(questions_lst)
-        exam_dict = questions_lst.sample(n=nb).to_dict(orient="records")
-        return exam_dict
-    except IndexError:
-        return {}
-
-
 class Question(BaseModel):
     question: str
     subject: str
@@ -81,5 +134,21 @@ async def put_question(question: Question):
     try:
         questions_dict.append(jsonable_encoder(question))
         return question
+    except IndexError:
+        return {}
+
+
+@api.get("/exam")
+async def get_exam(use: str, subject: str, nb: int):
+    try:
+        subject_lst = list(subject.split(","))
+        questions_db = pd.DataFrame(jsonable_encoder(questions_dict))
+        questions_lst = questions_db[
+            (questions_db["use"] == use) & (questions_db["subject"].isin(subject_lst))
+        ]
+        if nb > len(questions_lst):
+            nb = len(questions_lst)
+        exam_dict = questions_lst.sample(n=nb).to_dict(orient="records")
+        return exam_dict
     except IndexError:
         return {}
